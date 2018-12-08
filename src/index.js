4ca543ad11599d760e358311d3c6d267eb4c256a
Kent C. Dodds <kent+github@doddsfamily.us>
2018-12-08 14:14:29 -0700

    add file chooser

--- src/index.js
+++ src/index.js
@@ -18,6 +18,12 @@ function App() {
     [playbackRate],
   )
 
+  function setAudioSrcFromLocalFile(event) {
+    const reader = new FileReader()
+    reader.onload = e => setAudioSrc(e.target.result)
+    reader.readAsDataURL(event.target.files[0])
+  }
+
   return (
     <div>
       <Global
@@ -34,7 +40,7 @@ function App() {
           justifyContents: 'center',
         }}
       >
-        <h1>
+        <h1 css={{textAlign: 'center', fontSize: '3rem'}}>
           Audio Speeder{' '}
           <span
             role="img"
@@ -45,8 +51,19 @@ function App() {
           </span>
         </h1>
 
+        <div css={{fontSize: '1.5em', marginBottom: 20}}>Choose One:</div>
+
+        <label>Local File</label>
+        <input
+          css={{marginBottom: 20}}
+          type="file"
+          onChange={setAudioSrcFromLocalFile}
+        />
+
+        <label htmlFor="fileUrl">File URL</label>
         <input
           type="text"
+          id="fileUrl"
           onChange={e => setAudioSrc(e.target.value)}
           css={{
             lineHeight: 2,
@@ -54,19 +71,43 @@ function App() {
             padding: '6px 14px',
             borderRadius: 4,
             border: '2px solid #1d1075',
+            marginBottom: 20,
           }}
         />
         {audioSrc ? (
           <>
-            <input
-              type="range"
-              min={0}
-              max={5}
-              step="0.1"
-              defaultValue={playbackRate}
-              onChange={e => setPlaybackRate(e.target.value)}
-            />
-            <div>Playback Rate: {playbackRate}</div>
+            <div
+              css={{display: 'flex', alignItems: 'center', marginBottom: 20}}
+            >
+              <label css={{marginRight: 20, lineHeight: 1.5}}>
+                Playback Rate
+              </label>
+              <input
+                type="number"
+                min={0}
+                max={5}
+                step="0.1"
+                defaultValue={playbackRate}
+                onChange={e => setPlaybackRate(e.target.value)}
+                css={{
+                  lineHeight: 1.5,
+                  fontSize: '1.2rem',
+                  padding: '6px 14px',
+                  borderRadius: 4,
+                  border: '2px solid #1d1075',
+                  marginRight: 10,
+                }}
+              />
+              <input
+                type="range"
+                min={0}
+                max={5}
+                step="0.1"
+                defaultValue={playbackRate}
+                onChange={e => setPlaybackRate(e.target.value)}
+                css={{flex: '1'}}
+              />
+            </div>
             <audio
               controls={true}
               css={{width: '100%'}}
